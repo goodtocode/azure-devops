38a1e5c84566c5dfabbb036a273df410c67607f5
Robert Good <robert.good@goodtocode.com>
2023-05-28 19:58:20 -0700

    Versioning standards to set-version

--- scripts/Set-Version.ps1
+++ scripts/Set-Version.ps1
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------
-# Set-Version [-Path [<String>]] [-VersionToReplace [<String>]]
+# Set-Version [-Path [<String>]] [-VersionToReplace [<String>]]  [-Type [<String>]] 
 #
 # Example: .\Set-Version -Path \\source\path -Major 1
 #-----------------------------------------------------------------------
@@ -15,7 +15,10 @@ param
 	[String] $Major='-1',
 	[String] $Minor='-1',
 	[String] $Revision='-1',
-	[String] $Build='-1'
+	[String] $Build='-1',
+	[String] $PreRelease='-1',
+	[String] $CommitHash='-1',
+	[String] $Type='Assembly'
 )
 
 # ***
@@ -47,26 +50,38 @@ If($IsWindows){
 # ***
 # *** Execute
 # ***
-$Major = $Major.Replace('-1', $VersionToReplace.ToString().Substring(0,1))
-$Minor = $Minor.Replace('-1', (Get-Date -UFormat '%Y').ToString().Substring(2,2)) # YY
-$Revision = $Revision.Replace('-1', (Get-Date -UFormat '%j').ToString()) # DayOfYear
-$Build = $Build.Replace('-1', (Get-Date -UFormat '%H%M').ToString()) # HrMinSec
+$Major = $Major.Replace('-1', $VersionToReplace.ToString().Substring(0,1)) # Static 1, 2, 3
+$Minor = $Minor.Replace('-1', (Get-Date -UFormat '%Y').ToString().Substring(2,2)) # Year YYYY 2023
+$Revision = $Revision.Replace('-1', (Get-Date -UFormat '%j').ToString()) # DayOfYear D[DD]1-365
+$Build = $Build.Replace('-1', (Get-Date -UFormat '%H%M').ToString()) # HrMin 1937
+$Patch = $Patch.Replace('-1', (Get-Date -UFormat '%m').ToString()) # Month mm
+$PreRelease = $PreRelease.Replace('-1', '') # -alpha
+$CommitHash = $CommitHash.Replace('-1', '') # +204ff0a
 
-# .Net Projects
-$LongVersion = "$Major.$Minor.$Revision.$Build"
-$ShortVersion = "$Major.$Minor.$Revision"
-Write-Host "LongVersion: $LongVersion ShortVersion: $ShortVersion"
+
+# Version Formats
+$FileVersion = "$Major.$Minor.$Revision.$Build" # Ref: https://learn.microsoft.com/en-us/dotnet/standard/library-guidance/versioning
+$AssemblyVersion = "$Major.$Minor.0.0"
+$InformationalVersion = "$Major.$Minor.$Revision$PreRelease$CommitHash"
+$SemanticVersion = "$Major.$Minor.$Patch$PreRelease"
+Write-Host "FileVersion: $FileVersion SemanticVersion: $SemanticVersion"
+
 
 # Package.json version
-Update-LineByContains -Path $Path -Contains 'version' -Line """version"": ""$LongVersion""," -Include package.json
-# OpenApiConfigurationOptions.cs version
-Update-LineByContains -Path $Path -Contains 'Version' -Line "Version = ""$LongVersion""," -Include OpenApiConfigurationOptions.cs
-# *.csproj C# Project files
-Update-ContentsByTag -Path $Path -Value $LongVersion -Open '<Version>' -Close '</Version>' -Include *.csproj
-# *.nuspec NuGet packages
-Update-ContentsByTag -Path $Path -Value $LongVersion -Open '<version>' -Close '</version>' -Include *.nuspec
-# Assembly.cs C# assembly manifest
-Update-LineByContains -Path $Path -Contains "AssemblyVersion(" -Line "[assembly: AssemblyVersion(""$LongVersion"")]" -Include AssemblyInfo.cs
-# *.vsixmanifest VSIX Visual Studio Templates
-Update-TextByContains -Path $Path -Contains "<Identity Id" -Old $VersionToReplace -New $ShortVersion -Include *.vsixmanifest
+Update-LineByContains -Path $Path -Contains 'version' -Line """version"": ""$FileVersion""," -Include package.json
 
+# OpenApiConfigurationOptions.cs version
+Update-LineByContains -Path $Path -Contains 'Version' -Line "Version = ""$AssemblyVersion""," -Include OpenApiConfigurationOptions.cs
+
+# *.csproj C# Project files
+Update-ContentsByTag -Path $Path -Value $FileVersion -Open '<Version>' -Close '</Version>' -Include *.csproj
+
+# *.nuspec NuGet packages
+Update-ContentsByTag -Path $Path -Value $SemanticVersion -Open '<version>' -Close '</version>' -Include *.nuspec
+
+# Assembly.cs C# assembly manifest
+Update-LineByContains -Path $Path -Contains "FileVersion(" -Line "[assembly: FileVersion(""$FileVersion"")]" -Include AssemblyInfo.cs
+Update-LineByContains -Path $Path -Contains "AssemblyVersion(" -Line "[assembly: AssemblyVersion(""$AssemblyVersion"")]" -Include AssemblyInfo.cs
+
+# *.vsixmanifest VSIX Visual Studio Templates
+Update-TextByContains -Path $Path -Contains "<Identity Id" -Old $VersionToReplace -New $FileVersion -Include *.vsixmanifest
